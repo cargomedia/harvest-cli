1152e9ffbe08f5889f6de4917415a76492323be0
Reto Kaiser <reto@cargomedia.ch>
2014-06-08 22:31:09 +0200

    Change default config

--- resources/config/default.php
+++ resources/config/default.php
@@ -2,8 +2,8 @@
 
 return function (CM_Config_Node $config) {
     $config->Harvest_Api_Client->account = 'cargomedia';
-    $config->Harvest_Api_Client->email = 'ff@cargomedia.ch';
-    $config->Harvest_Api_Client->password = 'cargomedia123!';
+    $config->Harvest_Api_Client->email = 'hello@cargomedia.ch';
+    $config->Harvest_Api_Client->password = 'my-password';
 
     $config->Harvest_Cli->defaultProject = 2355141;
 };
